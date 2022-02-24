2ffcba1b0f42457a602fcb7ff2322ab751aa2676
Atcharawan B <80392458+MiiKiiza@users.noreply.github.com>
2022-02-24 10:36:19 +0700

    edit employee routes

--- routes/employee.js
+++ routes/employee.js
@@ -6,4 +6,32 @@ router.get('/test', async (req, res) => {
   res.status(200).send({ message: "hi" })
 })
 
+router.post('/login', async (req, res) => {
+
+})
+
+router.post('/add-employee', async (req, res) => {
+
+})
+
+router.post('/edit-employee', async (req, res) => {
+  
+})
+
+router.post('/delete-employee', async (req, res) => {
+  
+})
+
+router.post('/add-package', async (req, res) => {
+
+})
+
+router.post('/edit-package', async (req, res) => {
+
+})
+
+router.post('/check-verify-identity', async (req, res) => {
+
+})
+
 module.exports = router
